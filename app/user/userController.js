8128650ee8aec6fbc36781225705aa07bd92d51c
NamcoolNguyen <namndse02613@fpt.edu.vn>
2015-01-25 01:14:33 +0700

    update Userpage

--- app/user/userController.js
+++ app/user/userController.js
@@ -9,29 +9,29 @@
 
     function userController($scope, ngDialog, $routeParams, userService) {
         userService.getUser($routeParams.userId)
-            .success(function(data){
+            .success(function (data) {
                 $scope.user = data.user;
-                var countFollowing= 0,countFollower= 0,countPhoto=0;
-                for (var i=0; i < data.user.following.length; i+=1){
+                var countFollowing = 0, countFollower = 0, countPhoto = 0;
+                for (var i = 0; i < data.user.following.length; i += 1) {
                     countFollowing++;
                 }
-                for (var i=0; i < data.user.follower.length; i+=1){
+                for (var i = 0; i < data.user.follower.length; i += 1) {
                     countFollower++;
                 }
-                $scope.countFollowing=countFollowing;
-                $scope.countFollower=countFollower;
+                $scope.countFollowing = countFollowing;
+                $scope.countFollower = countFollower;
                 $scope.posts = [];
-                for (var i=0; i < data.user.posts.length; i+=1){
+                for (var i = 0; i < data.user.posts.length; i += 1) {
                     $scope.posts.push(data.user.posts[i]);
                     countPhoto++;
-                    if(i < 6){
-                        $scope.flagAmount=false;
+                    if (i < 6) {
+                        $scope.flagAmount = false;
                     }
                 }
-                $scope.countPhoto=countPhoto;
+                $scope.countPhoto = countPhoto;
                 console.log($scope.user);
             })
-            .error(function(){
+            .error(function () {
                 console.log(data);
             });
     }
