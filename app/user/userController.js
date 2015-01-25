99a9d36178986787167704eb06a60d64b3c707d1
Nghiavt <tuannghia1702@gmail.com>
2015-01-25 20:05:58 +0700

    Nghia: fix user

--- app/user/userController.js
+++ app/user/userController.js
@@ -11,27 +11,9 @@
         userService.getUser($routeParams.userId)
             .success(function (data) {
                 $scope.user = data.user;
-                var countFollowing = 0, countFollower = 0, countPhoto = 0;
-                for (var i = 0; i < data.user.following.length; i += 1) {
-                    countFollowing++;
-                }
-                for (var i = 0; i < data.user.follower.length; i += 1) {
-                    countFollower++;
-                }
-                $scope.countFollowing = countFollowing;
-                $scope.countFollower = countFollower;
-                $scope.posts = [];
-                for (var i = 0; i < data.user.posts.length; i += 1) {
-                    $scope.posts.push(data.user.posts[i]);
-                    countPhoto++;
-                    if (i < 6) {
-                        $scope.flagAmount = false;
-                    }
-                }
-                $scope.countPhoto = countPhoto;
-                console.log($scope.user);
+
             })
-            .error(function () {
+            .error(function (data) {
                 console.log(data);
             });
     }
