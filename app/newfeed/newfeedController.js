587a7b352920f59d12670516c0171797f2bd1679
Nghiavt <tuannghia1702@gmail.com>
2015-02-02 20:00:54 +0700

    Nghia: fix

--- app/newfeed/newfeedController.js
+++ app/newfeed/newfeedController.js
@@ -140,9 +140,8 @@
 
                             if ($scope.loginUser) {
                                 for (var i = 0; i < $scope.post.like.length; i++) {
-
+                                    console.log($scope.post.like[i].user_id, $scope.loginUser.id, $scope.post.like[i].user_id == $scope.loginUser.id)
                                     if ($scope.post.like[i].user_id == $scope.loginUser.id) {
-                                        console.log(123)
                                         $scope.iconLike = true;
                                     } else {
                                         $scope.iconLike = false;
