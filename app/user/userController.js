052383aa092378f5ee081ee60f1a6dc10650cece
NamcoolNguyen <namndse02613@fpt.edu.vn>
2015-02-07 16:25:19 +0700

    update follow

--- app/user/userController.js
+++ app/user/userController.js
@@ -8,15 +8,18 @@
     userController.$inject = ['$scope', 'ngDialog', '$routeParams', 'userService'];
 
     function userController($scope, ngDialog, $routeParams, userService) {
-        $scope.following = [];
-        $scope.follower = [];
+        $scope.flBtn = false;
         userService.getUser($routeParams.userId)
             .success(function (data) {
+                $scope.following = [];
+                $scope.follower = [];
                 $scope.user = data.user;
-                if (data.user.following.length != 0 && data.user.following.length != 0) {
+                if (data.user.following.length != 0) {
                     for (var i = 0; i < data.user.following.length; i += 1) {
                         $scope.following.push(data.user.following[i]);
                     }
+                }
+                if (data.user.follower.length != 0) {
                     for (var i = 0; i < data.user.follower.length; i += 1) {
                         $scope.follower.push(data.user.follower[i]);
                     }
@@ -26,5 +29,21 @@
             .error(function (data) {
                 console.log(data);
             });
+
+        $scope.addFollow = function (user) {
+            var data;
+            data = {
+                user_id: user.user_id,
+                follower_id: $routeParams.userId
+            };
+            userService.addFollow(data)
+                .success(function () {
+                    $scope.flBtn = true;
+                })
+                .error(function (data) {
+                    console.log(data);
+                })
+        }
+
     }
 })(angular);
