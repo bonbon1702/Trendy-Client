8d9e1c53cd8202c07fec7415b9f327248aab524d
nghiat <tuannghia1702@gmail.com>
2015-02-08 21:19:18 +0700

    Nghia: trending implement

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -9,26 +9,27 @@
 
     function homeController($scope, ngDialog, homeService) {
 
-        homeService.map()
-            .success(function (data) {
-                var map = [];
-                for (var i = 0; i < data.posts.length; i++) {
-                    if (data.posts[i] != null) {
-                        var item = {
-                            'caption': data.posts[i],
-                            'image_url': data.posts[i].image_url,
-                            'lat': data.posts[i].lat,
-                            'lang': data.posts[i].lang,
-                            'address': data.posts[i].address
-                        };
-                        map.push(item);
-                    }
-                }
-                googleMap.init(map);
-            })
-            .error(function (data) {
-                console.log(data);
-            });
+//        homeService.map()
+//            .success(function (data) {
+//                var map = [];
+//                for (var i = 0; i < data.posts.length; i++) {
+//                    if (data.posts[i] != null) {
+//                        var item = {
+//                            'caption': data.posts[i],
+//                            'image_url': data.posts[i].image_url,
+//                            'lat': data.posts[i].lat,
+//                            'lang': data.posts[i].lang,
+//                            'address': data.posts[i].address
+//                        };
+//                        map.push(item);
+//                    }
+//                }
+//                googleMap.init(map);
+//            })
+//            .error(function (data) {
+//                console.log(data);
+//            });
+        googleMap.init();
 
     }
 })(angular);
