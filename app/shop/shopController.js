4e7cb4a169f211e32b183c4072691bd12b32f101
trunganh2810 <anhlt.2828@gmail.com>
2015-03-08 11:38:04 +0700

    shop error

--- app/shop/shopController.js
+++ app/shop/shopController.js
@@ -7,7 +7,100 @@
 
     shopController.$inject = ['$scope', 'ngDialog', 'shopService'];
 
-    function shopController($scope, ngDialog, shopService) {
+    function shopController($scope, ngDialog, shopService, headerService) {
         googleMap.init();
+        $scope.comment = null;
+
+        headerService.loginUser()
+            .success(function (data) {
+                $scope.loginUser = data.user;
+            })
+            .error();
+
+        $scope.showDialog = function (id) {
+            ngDialog.open({
+                template: 'app/shop/templates/shop.html',
+                controller: ['$scope', 'shopService', 'window', 'headerService', function ($scope, shopService, $window, headerService) {
+                    $scope.iconLike = false;
+
+                    shopService.get(id)
+                        .success(function(data){
+                            $scope.shop = data.shop;
+
+                            headerService.loginUser()
+                                .success(function(data){
+                                    $scope.loginUser = data.user;
+                                    if ($scope.loginUser) {
+                                        for(var i = 0; i < $scope.shop.like.length; i++) {
+                                            if ($scope.shop.like[i].user_id == $scope.loginUser.id) {
+                                                $scope.iconLike = true;
+                                                break;
+                                            } else {
+                                                $scope.iconLike = false;
+                                            }
+                                        }
+                                    }
+                                })
+                                .error();
+
+                            $scope.submitComment = function () {
+                                if(!$scope.loginUser) {
+                                    event.preventDefault();
+                                    headerService.openLogin();
+                                } else {
+                                    var data = {
+                                        'content': $scope.comment,
+                                        'type_comment': 1,
+                                        'type_id': id,
+                                        'user_id': $scope.loginUser.id
+                                    };
+                                    $scope.comment = null;
+                                    shopService.save(data)
+                                        .success(function(data){
+                                            $scope.shop.comment.push(data.comment);
+                                        })
+                                        .error(function(data){
+
+                                        });
+                                }
+                            };
+
+                            $scope.likeOrDislike = function() {
+                                if(!$scope.loginUser) {
+                                    event.preventDefault();
+                                    headerService.openLogin();
+                                } else {
+                                    var data = {
+                                        'id': id,
+                                        type: $scope.iconLike == true ? 0 : 1,
+                                        user: $scope.loginUser.id
+                                    };
+
+                                    shopService.likeOrDislike(data)
+                                        .success(function (data) {
+                                            if ($scope.iconLike == true) {
+                                                $scope.shop.like.length--;
+                                                $scope.iconLike = false;
+                                            } else {
+                                                $scope.shop.like.length++;
+                                                $scope.iconLike = true;
+                                            }
+                                        })
+                                        .error(function(data){
+
+                                        });
+                                }
+                            };
+
+                            $scope.closeDialog = function(){
+                                ngDialog.close();
+                            }
+                        })
+                        .error(function(data){
+
+                        });
+                }]
+            })
+        }
     }
 })(angular);
--- app/shop/shopService.js
+++ app/shop/shopService.js
@@ -9,7 +9,26 @@
     function shopService($http, $rootScope) {
         return {
             list: function () {
-
+                return $http.get($rootScope.url + 'shop');
+            },
+            get: function(id) {
+                return $http.get($rootScope.url + 'shop/' + id);
+            },
+            save: function(data) {
+                return $http({
+                    method: 'POST',
+                    url: $rootScope.url + 'comment',
+                    data: data
+                })
+            },
+            getComments: function(id) {
+                return $http.get($rootScope.url + 'comment/showShop/' + id);
+            },
+            likeOrDislike: function(data) {
+                return $http.get($rootScope.url + 'like/likeShop/' + data.id + '/type/' + data.type + '/user/' + data.user);
+            },
+            countLike: function(id) {
+                return $http.get($rootScope.url + 'like/countLikeShop/' + id);
             }
         }
     }
