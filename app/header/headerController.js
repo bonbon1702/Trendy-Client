3d6f85527518b3679442d9bc5a4164b50e1bca94
bonbon1702 <tuannghia1702@gmail.com>
2015-03-23 02:11:50 +0700

    Nghia: vps

--- app/header/headerController.js
+++ app/header/headerController.js
@@ -31,7 +31,7 @@
         };
         hello.init({
             facebook: '903918442963068',
-            google: '103178250738-8o22armgdv5ej7ip215l4inmc1kvmqo9.apps.googleusercontent.com',
+            google: '785860817367-or5nbtrsppv2bm44nnfqeuf2t1qlffqj.apps.googleusercontent.com',
             twitter: '2518012026-WrP1ptaKi9jS3C84BMjqaqkdyjywX0Mfmpadp8Q'
         }, {
             scope: 'email'
